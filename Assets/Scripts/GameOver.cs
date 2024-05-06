a0c8b14b5010a2041f9de5efae454f8112178d65
Aditya <ad1315@scarletmail.rutgers.edu>
2024-05-06 14:24:20 -0400

    Functional win and lose screens

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -10,10 +10,12 @@ public class GameOver : MonoBehaviour {
     }
 
     public void Retry() {
+        //Debug.Log(SceneManager.GetActiveScene().buildIndex);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //restarts currently-loaded scene
     }
 
     public void Menu() {
+        /*TO-DO: Use SceneManager to load menu scene when pressed*/
         Debug.Log("Go to menu..");
     }
 }
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,25 +1,37 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class WaveSpawner : MonoBehaviour
 {
     public Transform enemyPrefab; //Prefab for enemytype 1
     public Transform spawnPoint;
-    public float timeBetweenWaves = 10f; //a new wave every 10 seconds
+    public float timeBetweenWaves = 15f; //a new wave every 10 seconds
 
     private Transform[] waypointTargets;
 
     private float countdown = 3f; //however long to take at beginning of level to start the first wave
                                   //3 seconds
-    private float timeBetweenWaveEnemy = 1f; //use this to space out enemies that spawn in one wave, !!prevents stacking!!
+    private float timeBetweenWaveEnemy = 1.3f; //use this to space out enemies that spawn in one wave, !!prevents stacking!!
     private int waveNumber = 0;
 
-    public int maxWaveCount = 5;
+    public int maxWaveCount = 5; // 5 shall be default
 
     void Awake() {
+        // Give a newly-spawned enemy its waypoints to follow
         waypointTargets = new Transform[transform.childCount];
-        for (int i = 0; i < waypointTargets.Length; i++) {
+        for(int i = 0; i < waypointTargets.Length; i++) {
             waypointTargets[i] = transform.GetChild(i);
+        }
+
+        // Set the maximum amount of waves to spawn depending on what level is loaded
+        // If level 1, set maxWaveCount to 6
+        if(SceneManager.GetActiveScene().buildIndex == GameManager.LEVEL_ONE_SCENE_INDEX) {
+            maxWaveCount = 6;
+        }
+        // If level 2, set maxWaveCount to 10
+        else if(SceneManager.GetActiveScene().buildIndex == GameManager.LEVEL_TWO_SCENE_INDEX) {
+            maxWaveCount = 10;
         }
     }
 
@@ -33,12 +45,15 @@ public class WaveSpawner : MonoBehaviour
 
             countdown -= Time.deltaTime; //reduce countdown by 1 every second
         }
+        else {
+            GameManager.WavesDone = true;
+        }
     }
 
     //Co-routine to space out spawning of enemies each wave
     IEnumerator SpawnWave() {
         waveNumber++;
-        for (int i = 0; i < waveNumber; i++) {
+        for (int i = 0; i < waveNumber; i ++) {
             SpawnEnemy();
             yield return new WaitForSeconds(timeBetweenWaveEnemy);
         }
@@ -53,5 +68,15 @@ public class WaveSpawner : MonoBehaviour
     void SpawnEnemy() {
         var instance = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
         instance.GetComponent<EnemyMovement>().WaypointTargets = waypointTargets;
+
+        // Refer to GameManager script to see scene indices for levels
+        // If Level 1, set speed to 2.5f
+        if(SceneManager.GetActiveScene().buildIndex == GameManager.LEVEL_ONE_SCENE_INDEX) {
+            instance.GetComponent<EnemyMovement>().speed = 2.5f;
+        }
+        // If Level 2, set speed to 5f
+        else if(SceneManager.GetActiveScene().buildIndex == GameManager.LEVEL_TWO_SCENE_INDEX) {
+            instance.GetComponent<EnemyMovement>().speed = 5f;
+        }
     }
 }
