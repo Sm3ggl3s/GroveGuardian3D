b645daca816a338d979df61b75f0609bf3ec233f
gulpiegator <tristin.moore@rocketmail.com>
2024-05-06 21:22:31 -0400

    fixed player bullet for real this time

--- Assets/Scripts/Player/PlayerBullet.cs
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -20,11 +20,11 @@ public class PlayerBullet : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         Transform target = other.transform;
-        if (target.CompareTag("Enemy")) {
+        if (other.CompareTag("Enemy")) {
             EnemyBasic enemy = target.GetComponent<EnemyBasic>();
             Debug.Log("Enemy Found" + enemy);
             if(enemy != null){
-                enemy.TakeDamage(damage, 1);
+                enemy.TakeDamage(damage, 1f);
                 Debug.Log("Enemy Hit");
             }
         }
